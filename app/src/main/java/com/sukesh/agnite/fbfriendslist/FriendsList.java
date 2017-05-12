00acc6a7569bdf1815ba4d56de6684d63b0e6519
Sukesh Panwar <1421037@gmail.com>
2017-05-12 14:27:15 +0530

    fb contact list fetch

---
+++ app/src/main/java/com/sukesh/agnite/fbfriendslist/FriendsList.java
@@ -0,0 +1,56 @@
+package com.sukesh.agnite.fbfriendslist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.facebook.login.LoginManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+public class FriendsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_list);
+
+        Intent intent = getIntent();
+        String jsondata = intent.getStringExtra("jsondata");
+
+        JSONArray friendslist;
+        ArrayList<String> friends = new ArrayList<String>();
+
+        try {
+            friendslist = new JSONArray(jsondata);
+            for (int l=0; l < friendslist.length(); l++) {
+                friends.add(friendslist.getJSONObject(l).getString("name"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, friends);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+/*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+                Intent intent=new Intent(FriendsList.this,MainActivity.this);
+                startActivity(intent);
+
+            }
+        });*/
+    }
+
+}
